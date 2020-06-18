af9e818d35200f1cf893a289c24aa0a55f916b7c
NaJ <naj10000@live.com>
2020-06-18 14:05:22 -0400

    setting up

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import './App.css';
+import Home from './components/Home';
+import Navbar from './components/NavBar'
+
+
+const App =() =>{
+  return (
+    <div className="App">
+    
+    </div>
+  );
+}
+
+export default App;
