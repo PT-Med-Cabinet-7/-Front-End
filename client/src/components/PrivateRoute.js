abe1225019e34d2aeabead3e2487ace2d4ac99b1
NaJ <naj10000@live.com>
2020-06-22 22:25:02 -0400

    making a new  selection form

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import Strains from '../components/strains/Strains'
+import Login from './Login'
 
 
 export const PrivateRoute = ({ component: Component, ...rest}) => {
